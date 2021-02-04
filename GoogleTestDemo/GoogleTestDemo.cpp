1e93ec678ea3323058d9a72e75f602cf1a7466d4
Abhijith Dev S <subhash.abhijith.dev@gmail.com>
2021-02-04 18:54:22 +0530

    calling actual func

--- GoogleTestDemo/GoogleTestDemo.cpp
+++ GoogleTestDemo/GoogleTestDemo.cpp
@@ -7,8 +7,7 @@
 class DatabaseConnector {
 
 public:
-	virtual bool login(std::string username, std::string password) { return false; }
-	virtual bool login2(std::string username, std::string password) { return false; }
+	virtual bool login(std::string username, std::string password) { return true; }
 	virtual bool logout(std::string username) { return true; }
 	virtual int fetchRecord() { return -1; }
 };
@@ -22,17 +21,9 @@ public:
 	}
 
 	int init(std::string username, std::string password) {
-		if (rand() % 2) {
-			if (dbc.login(username, password)) {
-				std::cout << "[CONNECTION SUCCESS]\n";
-				return 1;
-			}
-		}
-		else {
-			if (dbc.login2(username, password)) {
-				std::cout << "[CONNECTION SUCCESS]\n";
-				return 1;
-			}
+		if (dbc.login(username, password)) {
+			std::cout << "[CONNECTION SUCCESS]\n";
+			return 1;
 		}
 		std::cout << "[CONNECTION FAILURE]\n";
 		return 1;
@@ -50,7 +41,6 @@ public:
 	MOCK_METHOD0(fetchRecord, int());
 	MOCK_METHOD1(logout, bool(std::string username));
 	MOCK_METHOD2(login, bool(std::string username, std::string password));
-	MOCK_METHOD2(login2, bool(std::string username, std::string password));
 
 };
 
@@ -59,11 +49,12 @@ TEST(MyDBTest, LoginTest) {
 	// Arrange
 	MockDBC dbc;
 	MyDatabase db(dbc);
-	// Here there is no expectation that this call has to be made and the number of times of call is also not monitored.
-	// Instead, this will be invoked if the call (login) is invoked once or more, then it will return true by default.
+	DatabaseConnector actualConnector;
 
-	// This can be used when one function out of two is being called to do something.
-	ON_CALL(dbc, login(testing::_, testing::_)).WillByDefault(testing::Return(true));
+	// here, we can invoke a single function inside the actual implementation as well.
+	// ON_CALL(dbc, login(testing::_, testing::_)).WillByDefault(testing::Invoke(&actualConnector, &DatabaseConnector::login));
+
+	EXPECT_CALL(dbc, login(testing::_, testing::_)).Times(1).WillOnce(testing::Invoke(&actualConnector, &DatabaseConnector::login));
 
 	//Act
 	auto ret = db.init("USerName", "Password");
