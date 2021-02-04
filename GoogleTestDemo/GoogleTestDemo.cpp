7bcbebee420da5cb0d93ccef4f3711815eb0d361
Abhijith Dev S <subhash.abhijith.dev@gmail.com>
2021-02-04 18:17:16 +0530

    usage

--- GoogleTestDemo/GoogleTestDemo.cpp
+++ GoogleTestDemo/GoogleTestDemo.cpp
@@ -8,6 +8,7 @@ class DatabaseConnector {
 
 public:
 	virtual bool login(std::string username, std::string password) { return false; }
+	virtual bool login2(std::string username, std::string password) { return false; }
 	virtual bool logout(std::string username) { return true; }
 	virtual int fetchRecord() { return -1; }
 };
@@ -21,9 +22,17 @@ public:
 	}
 
 	int init(std::string username, std::string password) {
-		if (dbc.login(username, password)) {
-			std::cout << "[CONNECTION SUCCESS]\n";
-			return 1;
+		if (rand() % 2) {
+			if (dbc.login(username, password)) {
+				std::cout << "[CONNECTION SUCCESS]\n";
+				return 1;
+			}
+		}
+		else {
+			if (dbc.login2(username, password)) {
+				std::cout << "[CONNECTION SUCCESS]\n";
+				return 1;
+			}
 		}
 		std::cout << "[CONNECTION FAILURE]\n";
 		return 1;
@@ -41,6 +50,7 @@ public:
 	MOCK_METHOD0(fetchRecord, int());
 	MOCK_METHOD1(logout, bool(std::string username));
 	MOCK_METHOD2(login, bool(std::string username, std::string password));
+	MOCK_METHOD2(login2, bool(std::string username, std::string password));
 
 };
 
@@ -51,6 +61,8 @@ TEST(MyDBTest, LoginTest) {
 	MyDatabase db(dbc);
 	// Here there is no expectation that this call has to be made and the number of times of call is also not monitored.
 	// Instead, this will be invoked if the call (login) is invoked once or more, then it will return true by default.
+
+	// This can be used when one function out of two is being called to do something.
 	ON_CALL(dbc, login(testing::_, testing::_)).WillByDefault(testing::Return(true));
 
 	//Act
