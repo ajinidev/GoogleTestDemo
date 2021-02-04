1a582ec519d467a951950c3b5b76d6a7efe490d6
Abhijith Dev S <subhash.abhijith.dev@gmail.com>
2021-02-04 17:06:29 +0530

    assersions

--- GoogleTestDemo/GoogleTestDemo.cpp
+++ GoogleTestDemo/GoogleTestDemo.cpp
@@ -1,14 +1,24 @@
 #include "pch.h"
-#include <iostream>
 #include <gtest/gtest.h>
 
-
-TEST(TestCaseName, SubTestName_1) {
+TEST(TestName, SubTestName_1) {
 	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
-}
+	EXPECT_TRUE(false);		// Assertion Failed. But execution will continue.
 
-TEST(TestCaseName, SubTestName_2) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+	ASSERT_FALSE(1 == 1);	// This will stop the execution [fatal assertion]
+
+	std::cout << "this will not be printed";
+
+	/*
+		Other macros
+
+		Equal				EXPECT_EX	ASSERT_EX
+		NotEqual			EXPECT_NE	ASSERT_NE
+		Less Than			EXPECT_LT	ASSERT_LT
+		Less than Equal		EXPECT_LE	ASSERT_LE
+		Greater Than		EXPECT_GT	ASSERT_GT
+		Greater Than Equal	EXPECT_GE	ASSERT_GE
+
+		Best practice : Keep single assersion in a test case even though multiple assersions can be added in one TC
+	*/
 }
